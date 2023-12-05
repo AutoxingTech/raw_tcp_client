f8d457cad641c527ee774832ddc1fe884c8dd8a0
liudf <liudf@autoxing.com>
2023-12-05 12:15:12 +0800

    add some commons; remove debug log

--- src/ros/message_wrapper.h
+++ src/ros/message_wrapper.h
@@ -40,19 +40,21 @@ struct __attribute__((packed)) WrapperHeader
 template <typename MessageType>
 void to_buffer(const MessageType& msg, std::vector<char>& buffer)
 {
+    // buffer: old data + (WrapperHeader + new msg data)
+    size_t header_size = sizeof(WrapperHeader);
     uint32_t msg_length = ros::serialization::serializationLength(msg);
     size_t old_size = buffer.size();
-    buffer.resize(old_size + 8 + msg_length);
+    buffer.resize(old_size + sizeof(WrapperHeader) + msg_length);
 
     WrapperHeader wrapper_header;
     wrapper_header.magic[0] = MessageType::magic_header[0];
     wrapper_header.magic[1] = MessageType::magic_header[1];
     wrapper_header.data_length = msg_length;
 
-    ros::serialization::OStream stream((uint8_t*)&buffer[old_size + 8], msg_length);
+    ros::serialization::OStream stream((uint8_t*)&buffer[old_size + header_size], msg_length);
     ros::serialization::serialize(stream, msg);
 
-    wrapper_header.crc16 = calculateCRC16(&buffer[old_size + 8], msg_length);
+    wrapper_header.crc16 = calculateCRC16(&buffer[old_size + header_size], msg_length);
     memcpy(&buffer[old_size], &wrapper_header, sizeof(wrapper_header));
 }
 
@@ -61,33 +63,28 @@ bool from_buffer(MessageType& msg, const char* buffer, size_t buffer_size)
 {
     if (buffer[0] != MessageType::magic_header[0] || buffer[1] != MessageType::magic_header[1])
     {
-        printf("1\n");
         return false;
     }
 
-    if (buffer_size < 8)
+    if (buffer_size < sizeof(WrapperHeader))
     {
-        printf("2\n");
         return false;
     }
 
     WrapperHeader* wrapper_header = (WrapperHeader*)buffer;
-    if (buffer_size < 8 + wrapper_header->data_length)
+    if (buffer_size < sizeof(WrapperHeader) + wrapper_header->data_length)
     {
-        printf("3\n");
         return false;
     }
 
     if (sizeof(WrapperHeader) != 8)
     {
-        printf("5\n");
         return false;
     }
 
     uint16_t crc16 = calculateCRC16(buffer + sizeof(WrapperHeader), wrapper_header->data_length);
     if (crc16 != wrapper_header->crc16)
     {
-        printf("4\n");
         return false;
     }
 
